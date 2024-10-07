f660fa7a7365e3792dd1153d8d6c48236404d9af
ajaymishraadt <133745035+ajaymishraadt@users.noreply.github.com>
2024-10-07 09:37:11 +0530

    Create Element.java

---
+++ Element.java
@@ -0,0 +1,18 @@
+
+public class Element{
+    public String maxOccurs;
+    public String minOccurs;
+    public String name;
+    public String read;
+    public String regex;
+    public String sequence;
+    public ArrayList<Element> subElements;
+}
+
+public class Message{
+    public ArrayList<Element> elements;
+}
+
+
+
+
